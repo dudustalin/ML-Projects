737b53301517dc8009f1a2c090d47b0eaf061ef3
Eduardo Cruz <dudustalin@gmail.com>
2022-09-18 22:24:32 -0300

    first commit

---
+++ life.py
@@ -0,0 +1,9 @@
+import math
+
+theAnswer = 42
+
+def quote_marvin():
+    print("I've calculated your chance of survival, but i=I don't thin yo'll like it.")
+
+def raiz_quadrada(x):
+    return math.sqrt(x)
